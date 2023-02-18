614bb7695fde2001f52d09ed62da4348738b5583
Marin Mucuta <50747955+pilot01j@users.noreply.github.com>
2023-02-18 15:04:08 +0200

    Add files via upload

---
+++ Day_16_PrettyTable/main.py
@@ -0,0 +1,17 @@
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Carmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "l"
+print(table)
